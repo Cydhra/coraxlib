bbc70dde26d7a0d7221e6cf9f03f4a6234775ad1
Alexey Kozlov <alexey.kozlov@h-its.org>
2021-04-13 03:37:02 +0200

    add optimize pll-module (WIP, will not compile yet)

---
+++ src/corax/optimize/opt_model.h
@@ -0,0 +1,114 @@
+/*
+    Copyright (C) 2016 Diego Darriba
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact: Diego Darriba <Diego.Darriba@h-its.org>,
+    Exelixis Lab, Heidelberg Instutute for Theoretical Studies
+    Schloss-Wolfsbrunnenweg 35, D-69118 Heidelberg, Germany
+*/
+#ifndef CORAX_OPTIMIZE_MODEL_H_
+#define CORAX_OPTIMIZE_MODEL_H_
+
+#include "opt_generic.h"
+
+#define PLLMOD_ALGO_MIN_WEIGHT_RATIO 0.001
+#define PLLMOD_ALGO_MAX_WEIGHT_RATIO    10
+#define PLLMOD_ALGO_BFGS_FACTR         1e9
+
+// it's actually defined in lbfgsb.h, but not exported from the optimize module
+#define PLLMOD_ALGO_LBFGSB_ERROR       1.0e-4
+
+#ifdef DEBUG
+    #define DBG(fmt, ...) do { printf(fmt, ##__VA_ARGS__); } while(0)
+#else
+    #define DBG(fmt, ...)
+#endif
+
+/*
+ * Optimize stationary frequencies for parameters `params_index`.
+ */
+PLL_EXPORT double pllmod_algo_opt_frequencies (pll_partition_t * partition,
+                                               pll_unode_t * tree,
+                                               unsigned int params_index,
+                                               unsigned int * params_indices,
+                                               double bfgs_factor,
+                                               double tolerance);
+
+/*
+ * Optimize substitution rates for parameters `params_index`.
+ * symmetries is an array with as many positions as substitution parameters,
+ *            or 'NULL' for GTR (e.g., for DNA, 012345 and NULL are equivalent)
+ *            Must be sorted and start with '0'.
+ *            e.g., 000000 = JC/F81, 010010 = K80/HKY, 012314 = TrN
+ */
+PLL_EXPORT double pllmod_algo_opt_subst_rates (pll_partition_t * partition,
+                                               pll_unode_t * tree,
+                                               unsigned int params_index,
+                                               unsigned int * params_indices,
+                                               int * symmetries,
+                                               double min_rate,
+                                               double max_rate,
+                                               double bfgs_factor,
+                                               double tolerance);
+
+PLL_EXPORT double pllmod_algo_opt_alpha (pll_partition_t * partition,
+                                         pll_unode_t * tree,
+                                         unsigned int * params_indices,
+                                         double min_alpha,
+                                         double max_alpha,
+                                         double *alpha,
+                                         double tolerance);
+
+PLL_EXPORT double pllmod_algo_opt_pinv (pll_partition_t * partition,
+                                        pll_unode_t * tree,
+                                        unsigned int * params_indices,
+                                        double min_pinv,
+                                        double max_pinv,
+                                        double tolerance);
+
+PLL_EXPORT double pllmod_algo_opt_alpha_pinv (pll_partition_t * partition,
+                                              pll_unode_t * tree,
+                                              unsigned int * params_indices,
+                                              double min_alpha,
+                                              double max_alpha,
+                                              double *alpha,
+                                              double min_pinv,
+                                              double max_pinv,
+                                              double bfgs_factor,
+                                              double tolerance);
+
+/*
+ * Optimize free rates and rate weights together, linked to `partition->rate_cats`.
+ * Uses 2 step L-BFGS-B algorithm.
+ */
+PLL_EXPORT double pllmod_algo_opt_rates_weights (pll_partition_t * partition,
+                                                 pll_unode_t * tree,
+                                                 unsigned int * params_indices,
+                                                 double min_rate,
+                                                 double max_rate,
+                                                 double bfgs_factor,
+                                                 double tolerance,
+                                                 double * brlen_scaler,
+                                                 int scale_branches);
+
+PLL_EXPORT double pllmod_algo_opt_brlen_scaler (pll_partition_t * partition,
+                                                pll_unode_t * tree,
+                                                unsigned int * params_indices,
+                                                double * scaler,
+                                                double min_scaler,
+                                                double max_scaler,
+                                                double tolerance);
+
+#endif /* CORAX_OPTIMIZE_MODEL_H_ */
