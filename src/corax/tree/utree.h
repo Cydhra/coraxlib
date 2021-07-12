526073f685fed5f8486c79458b2d1c4f228835d6
computations <bwbettisworth@gmail.com>
2021-07-12 11:40:38 +0200

    Parital fix of issue #7
    - Fix the documentation generation
    - Converted some of the pll_utree_t.md documentation to be inline
    - Converted the pll_operation_t.md to inline documenation
    - Convert the pll_partition_t.md to inline documentation.
    - Converted the documentation in pll_partition_t.md to inline
    - Remove the portion of the old docs which is now inline
    - Convert the pllmod_treeinfo_t.md documentation to inline.
    - Remove redundant documentation. Inline wins

--- src/corax/tree/utree.h
+++ src/corax/tree/utree.h
@@ -1,8 +1,21 @@
+/**
+ * @file utree.h
+ *
+ * @brief This header file contains functions related to the pll_utree_t struct
+ *
+ * @author whoever
+ */
 #ifndef CORAX_TREE_UTREE_H_
 #define CORAX_TREE_UTREE_H_
 
 #include "corax/corax_common.h"
 
+/** 
+ * Deallocate the memory associated with a utree. `cb_destroy` is used to delete
+ * the user data allocated in data.
+ *
+ * @ingroup pll_utree_t
+ */
 PLL_EXPORT void pll_utree_destroy(pll_utree_t *tree,
                                   void (*cb_destroy)(void *));
 
@@ -12,6 +25,18 @@ PLL_EXPORT void pll_utree_reset_template_indices(pll_unode_t *node,
 PLL_EXPORT void pll_utree_graph_destroy(pll_unode_t *root,
                                         void (*cb_destroy)(void *));
 
+/**
+ * Takes a tree, represented by a node, and optionally a tip count. Will produce
+ * a pll_utree_t that contains that tree. The pointer to the original node is
+ * not invalidated.
+ *
+ * @param root Pointer to the virtual root. Should be an "inner node".
+ *
+ * @param tip_count Number of tips in contained in the tree represented by
+ * `root`
+ *
+ * @ingroup pll_utree_t
+ */
 PLL_EXPORT pll_utree_t *pll_utree_wraptree(pll_unode_t *root,
                                            unsigned int tip_count);
 
@@ -28,6 +53,33 @@ PLL_EXPORT int pll_unode_is_rooted(const pll_unode_t *root);
 
 PLL_EXPORT int pll_utree_is_rooted(const pll_utree_t *tree);
 
+/**
+ * Given the `pll_unode_t**` from a traversal using `pll_utree_traverse`, this
+ * will create a list of `pll_operation_t`.
+ *
+ * @param trav_buffer
+ *
+ * @param trav_buffer_size
+ *
+ * @param[out] branches A buffer to store the branch length parameters used in
+ * the operations. Optional.
+ *
+ * @param[out] pmatrix_indices A buffer to store the pmatrix indices used in the
+ * operations. Optional.
+ *
+ * @param[out] ops Buffer to store the created ops. For a full traversal, the
+ * allocated size should be equal to the number of the number of branches in the
+ * tree.
+ *
+ * @param[out] matrix_count Out parameter indicating the number matrices
+ * required to perform the operations
+ *
+ * @param[out] ops_count Out parameter indicating the actual number of
+ * operations.
+ *
+ * @ingroup pll_utree_t
+ * @ingroup pll_operation_t
+ */
 PLL_EXPORT void pll_utree_create_operations(pll_unode_t *const *trav_buffer,
                                             unsigned int     trav_buffer_size,
                                             double *         branches,
@@ -40,6 +92,12 @@ PLL_EXPORT int pll_utree_check_integrity(const pll_utree_t *root);
 
 PLL_EXPORT pll_unode_t *pll_utree_graph_clone(const pll_unode_t *root);
 
+/**
+ * Clone a tree. This is a semi-deep copy. The fields `label` and the pointers
+ * `next` and `back` are deep copied, but the data field is shallowly copied.
+ *
+ * @param root The tree to clone.
+ */
 PLL_EXPORT pll_utree_t *pll_utree_clone(const pll_utree_t *root);
 
 PLL_EXPORT int pll_utree_set_clv_minimal(pll_unode_t *root,
--- src/corax/tree/utree_traverse.h
+++ src/corax/tree/utree_traverse.h
@@ -3,6 +3,45 @@
 
 #include "corax/corax_common.h"
 
+/** 
+ * Creates a list of nodes from a traversal, starting at root. The order of the
+ * traversal can be controlled with the traversal argument, which accepts either
+ * `PLL_TREE_TRAVERSE_POSTORDER `or `PLL_TREE_TRAVERSE_PREORDER`. The callback
+ * function controls which nodes are traversed by returning true for a node node
+ * which should be added to `outbuffer`. If false is returned instead, then the
+ * traversal is halted for that subtree, and the node which returned it is not
+ * added to `outbuffer`. By doing this the traversal of the tree can be halted
+ * early, which is useful for partial traversals. `trav_size `is an out
+ * parameter.  Returns `PLL_SUCCESS `on a traversal without errors, and
+ * `PLL_FAILURE `if there was an error.
+ *
+ * If a full traversal is desired, the callback should return true for all
+ * inputs.
+ *
+ * The intended use of this function is to build a traversal buffer, which is to
+ * be used in later computations. For example, the `outbuffer `parameter is used
+ * to build the operations for likelihood computations (see
+ * `pll_utree_create_operations`). The function will fail if `nullptr `is passed
+ * in for `outbuffer`. If the behavior of this function is needed, but the
+ * buffer is not needed, please use `pll_utree_traverse_apply`.
+ *
+ * @param root Node which the traversal will start from.
+ *
+ * @param traversal Type of traversal. Can be `PLL_TREE_TRAVERSE_POSTORDER` or
+ * `PLL_TREE_TRAVERSE_PREORDER`
+ *
+ * @param cbtrav Callback function which can be used to control the traversal.
+ * Please see the general function documentation for more detail.
+ *
+ * @param[out] outbuffer Buffer which will contain the traversed nodes in order.
+ *
+ * @param[out] trav_size Pointer a buffer which will contain the final traversal
+ * size.
+ *
+ * @return `PLL_SUCCESS` on a traversal without errors. `PLL_FAILURE` otherwise.
+ *
+ * @ingroup pll_utree_t
+ */
 PLL_EXPORT int pll_utree_traverse(pll_unode_t *root,
                                   int          traversal,
                                   int (*cbtrav)(pll_unode_t *),
