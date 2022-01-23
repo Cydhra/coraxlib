7f82bb706925fe47954bd97a669b7980924898d2
computations <bwbettisworth@gmail.com>
2022-01-23 12:28:52 +0100

    add the new functions to the header

--- src/corax/kernel/generic/core_pmatrix.h
+++ src/corax/kernel/generic/core_pmatrix.h
@@ -10,23 +10,36 @@ extern "C"
 
   /* functions in core_pmatrix.c */
 
-  CORAX_EXPORT int corax_core_update_pmatrix(double **           pmatrix,
+  CORAX_EXPORT int corax_core_update_pmatrix(double            **pmatrix,
                                              unsigned int        states,
                                              unsigned int        rate_cats,
-                                             const double *      rates,
-                                             const double *      branch_lengths,
+                                             const double       *rates,
+                                             const double       *branch_lengths,
                                              const unsigned int *matrix_indices,
                                              const unsigned int *params_indices,
-                                             const double *      prop_invar,
-                                             double *const *     eigenvals,
-                                             double *const *     eigenvecs,
-                                             double *const *     inv_eigenvecs,
+                                             const double       *prop_invar,
+                                             double *const      *eigenvals,
+                                             double *const      *eigenvecs,
+                                             double *const      *inv_eigenvecs,
                                              unsigned int        count,
                                              unsigned int        attrib);
+#ifdef CORAX_NONREV
+  CORAX_EXPORT int
+  corax_core_update_pmatrix_nonrev(double            **pmatrix,
+                                   size_t              states,
+                                   size_t              rate_cats,
+                                   const double       *rates,
+                                   const double       *branch_lengths,
+                                   const unsigned int *matrix_indices,
+                                   const unsigned int *params_indices,
+                                   const double       *prop_invar,
+                                   double *const      *params,
+                                   unsigned int        count,
+                                   unsigned int        attrib);
+#endif // CORAX_NONREV
 
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
-
 
 #endif /* CORAX_KERNEL_GENERIC_CORE_PMATRIX_H_ */
