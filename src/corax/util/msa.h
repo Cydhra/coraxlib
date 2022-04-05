e2706c13d376ba2eb60d2fcc842384f499591924
Julia <julaschmid@googlemail.com>
2022-04-05 13:52:39 +0200

    Add difficulty prediction
    - coraxlib msa.c includes entropy and bollback computation
    - prediction code included as submodule with optional build
    - tests for the new functions in msa.c included

--- src/corax/util/msa.h
+++ src/corax/util/msa.h
@@ -33,6 +33,7 @@
 #define CORAX_MSA_STATS_INV_COLS (1ul << 6)
 #define CORAX_MSA_STATS_FREQS (1ul << 7)
 #define CORAX_MSA_STATS_SUBST_RATES (1ul << 8)
+#define CORAX_MSA_STATS_ENTROPY (1ul << 9)
 #define CORAX_MSA_STATS_ALL (~0ul)
 
 #define CORAX_MSA_MAX_ERRORS 100
@@ -69,6 +70,9 @@ typedef struct msa_stats
 
   double *freqs;
   double *subst_rates;
+
+  double *column_entropies;
+  double entropy;
 } corax_msa_stats_t;
 
 typedef struct msa_errors
@@ -91,6 +95,51 @@ extern "C"
   corax_msa_empirical_subst_rates(const corax_partition_t *partition);
   CORAX_EXPORT double
   corax_msa_empirical_invariant_sites(corax_partition_t *partition);
+
+  /**
+     * Takes a multiple sequence alignment, the number of states and a tipmap and
+     * computes the Shannon entropies per alignment site (in bits).
+     *
+     * @param msa Multiple Sequence Alignment
+     * @param states Number of states (e.g., DNA=4, AA=20 etc.)
+     * @param tipmap Mapping from chars to states (e.g., corax_map_nt for DNA)
+     * @return Array of per-site Shannon entropies (in bits). Each per-site entropy is >= 0.
+   */
+  CORAX_EXPORT double *
+  corax_msa_column_entropies(const corax_msa_t *   msa,
+                             unsigned int          states,
+                             const corax_state_t * tipmap
+  );
+
+  /**
+   * Takes a multiple sequence alignment, the number of states and a tipmap and
+   * compute the Shannon entropy of the alignment.
+   *
+   * @param msa Multiple Sequence Alignment
+   * @param states Number of states (e.g., DNA=4, AA=20 etc.)
+   * @param tipmap Mapping from chars to states (e.g., corax_map_nt for DNA)
+   * @return Shannon entropy of the multiple sequence alignment (in bits). The entropy is >= 0.
+   */
+  CORAX_EXPORT double
+  corax_msa_entropy(const corax_msa_t *         msa,
+                    unsigned int          states,
+                    const corax_state_t * tipmap
+  );
+
+  /**
+   * Takes a multiple sequence alignment and a tipmap and computes the bollback multinomial
+   * statistic according to Bollback, JP: Bayesian model adequacy and choice in phylogenetics (2002).
+   *
+   * @param msa Multiple Sequence Alignment. Note that the MSA object is modified during
+   *            the computation of the multinomial statistic.
+   * @param tipmap Mapping from chars to states (e.g., corax_map_nt for DNA)
+   * @return Bollback multinomial statistic. The bollback multinomial statistic is always <= 0.
+   */
+  CORAX_EXPORT double
+  corax_msa_bollback_multinomial(corax_msa_t *         msa,
+                                 unsigned int *        site_weights,
+                                 const corax_state_t * tipmap
+  );
 
   CORAX_EXPORT corax_msa_errors_t *corax_msa_check(const corax_msa_t *  msa,
                                                    const corax_state_t *tipmap);
