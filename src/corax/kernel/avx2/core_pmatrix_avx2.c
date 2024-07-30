1d35765e18121ed8fab4ca207d2d4375696796c1
Alexey Kozlov <alexey.kozlov@h-its.org>
2024-07-30 14:38:50 +0200

    feat(kernels): add AVX2-vectorized pmatrix kernel for arbitrary number of states

--- src/corax/kernel/avx2/core_pmatrix_avx2.c
+++ src/corax/kernel/avx2/core_pmatrix_avx2.c
@@ -276,3 +276,217 @@ int corax_core_update_pmatrix_20x20_avx2(double **           pmatrix,
   free(tran_evecs);
   return CORAX_SUCCESS;
 }
+
+CORAX_EXPORT
+int corax_core_update_pmatrix_avx2(double **           pmatrix,
+                                   unsigned int        states,
+                                   unsigned int        rate_cats,
+                                   const double *      rates,
+                                   const double *      branch_lengths,
+                                   const unsigned int *matrix_indices,
+                                   const unsigned int *params_indices,
+                                   const double *      prop_invar,
+                                   double *const *     eigenvals,
+                                   double *const *     eigenvecs,
+                                   double *const *     inv_eigenvecs,
+                                   unsigned int        count)
+{
+  unsigned int i, n, j, k, m;
+  double       pinvar;
+
+  int *    transposed;
+  double * evecs;
+  double * inv_evecs;
+  double * evals;
+  double * pmat;
+  double * expd;
+  double * temp;
+  double **tran_evecs;
+
+  unsigned int states_padded = (states + 3) & 0xFFFFFFFC;
+  unsigned int states_padded_squared = states_padded * states_padded;
+
+  expd =
+      (double *)corax_aligned_alloc(states_padded * sizeof(double), CORAX_ALIGNMENT_AVX);
+  temp =
+      (double *)corax_aligned_alloc(states_padded_squared * sizeof(double), CORAX_ALIGNMENT_AVX);
+
+  /* transposed eigen vectors */
+  transposed = (int *)calloc((size_t)rate_cats, sizeof(int));
+  tran_evecs = (double **)calloc((size_t)rate_cats, sizeof(double *));
+
+  if (!expd || !temp || !transposed || !tran_evecs)
+  {
+    if (expd) corax_aligned_free(expd);
+    if (temp) corax_aligned_free(temp);
+    if (transposed) free(transposed);
+    if (tran_evecs) free(tran_evecs);
+
+    corax_set_error(CORAX_ERROR_MEM_ALLOC, "Unable to allocate enough memory.");
+    return CORAX_FAILURE;
+  }
+
+  /* transpose eigenvectors */
+  /* TODO: The same trick can be applied for exponentiations */
+  for (n = 0; n < rate_cats; ++n)
+  {
+    int index = params_indices[n];
+
+    if (!transposed[index])
+    {
+      /* allocate space for transposed eigenvectors and check that
+         allocation succeeds */
+      double *tran = (double *)corax_aligned_alloc(states_padded_squared * sizeof(double),
+                                                   CORAX_ALIGNMENT_AVX);
+      if (!tran)
+      {
+        corax_aligned_free(expd);
+        corax_aligned_free(temp);
+        free(transposed);
+        for (i = 0; i < n; ++i)
+          if (tran_evecs[i]) corax_aligned_free(tran_evecs[i]);
+        free(tran_evecs);
+
+        corax_set_error(CORAX_ERROR_MEM_ALLOC,
+                        "Unable to allocate enough memory.");
+        return CORAX_FAILURE;
+      }
+
+      /* transpose eigen vectors */
+      evecs = eigenvecs[index];
+      for (i = 0; i < states; ++i)
+      {
+        for (j = 0; j < states; ++j)
+          tran[i * states_padded + j] = evecs[j * states_padded + i];
+      }
+
+      /* update pointers and indicate that the eigen vector for the current
+         rate matrix with index was updated */
+      tran_evecs[index] = tran;
+      transposed[index] = 1;
+    }
+  }
+  free(transposed);
+
+  __m256d xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6;
+  __m256d ymm0;
+  __m256d zmm0, zmm1;
+
+  double *tran = NULL;
+  for (i = 0; i < count; ++i)
+  {
+    assert(branch_lengths[i] >= 0);
+
+    xmm3 = _mm256_set1_pd(branch_lengths[i]);
+    pmat = pmatrix[matrix_indices[i]];
+
+    /* compute effective pmatrix location */
+    for (n = 0; n < rate_cats; ++n)
+    {
+      int index = params_indices[n];
+      pinvar    = prop_invar[index];
+      tran      = tran_evecs[index];
+      inv_evecs = inv_eigenvecs[index];
+      evals     = eigenvals[index];
+      evecs     = eigenvecs[index];
+
+      /* if branch length is zero then set the p-matrix to identity matrix */
+      if (!branch_lengths[i])
+      {
+        xmm0 = _mm256_setzero_pd();
+        for (j = 0; j < states; ++j)
+        {
+          for (k = 0; j < states_padded; j += 4)
+            _mm256_store_pd(pmat + k, xmm0);
+          pmat[j] = 1;
+          pmat += states_padded;
+        }
+        continue;
+      }
+
+      /* exponentiate eigenvalues */
+      xmm2 = _mm256_set1_pd(rates[n]);
+
+      if (pinvar > CORAX_MISC_EPSILON) xmm6 = _mm256_set1_pd(1.0 - pinvar);
+
+      for (k = 0; k < states_padded; k += 4)
+      {
+        xmm1 = _mm256_load_pd(evals + k);
+
+        /* scalar multiplication with rates */
+        xmm4 = _mm256_mul_pd(xmm1, xmm2);
+
+        /* scalar multiplication with branch lengths */
+        xmm5 = _mm256_mul_pd(xmm4, xmm3);
+
+        if (pinvar > CORAX_MISC_EPSILON) { xmm5 = _mm256_div_pd(xmm5, xmm6); }
+
+        _mm256_store_pd(expd + k, xmm5);
+      }
+
+      /* NOTE: in order to deal with numerical issues in cases when Qt -> 0, we
+       * use a trick suggested by Ben Redelings and explained here:
+       * https://github.com/xflouris/libpll/issues/129#issuecomment-304004005
+       * In short, we use expm1() to compute (exp(Qt) - I), and then correct
+       * for this by adding an identity matrix I in the very end */
+
+      for (k = 0; k < states; ++k) expd[k] = expm1(expd[k]);
+
+      /* compute temp matrix */
+      unsigned int offset = 0;
+      for (j = 0; j < states; ++j)
+      {
+        for (k = 0; k < states_padded; k += 4)
+        {
+          xmm0 = _mm256_load_pd(expd + k);
+          ymm0 = _mm256_load_pd(inv_evecs + offset + k);
+          zmm0 = _mm256_mul_pd(xmm0, ymm0);
+          _mm256_store_pd(temp + offset + k, zmm0);
+        }
+        offset += states_padded;
+      }
+
+      offset = 0;
+      for (j = 0; j < states; ++j)
+      {
+        unsigned int offset2 = 0;
+        for (k = 0; k < states; ++k)
+        {
+          zmm0 = _mm256_setzero_pd();
+
+          for (m = 0; m < states_padded; m += 4)
+          {
+            xmm0 = _mm256_load_pd(temp + offset + m);
+            ymm0 = _mm256_load_pd(tran + offset2 + m);
+            zmm0 = _mm256_fmadd_pd(xmm0, ymm0, zmm0);                                          \
+          }
+
+          /* add up four elements of zmm0 */
+          zmm1    = _mm256_hadd_pd(zmm0, zmm0);
+          pmat[k] = ((double *)&zmm1)[0] + ((double *)&zmm1)[2];
+          offset2 += states_padded;
+        }
+        pmat += states_padded;
+        offset += states_padded;
+      }
+
+      /* add identity matrix */
+      pmat -= states * states_padded;
+      for (j = 0; j < states; ++j)
+      {
+        pmat[j] += 1.0;
+        pmat += states_padded;
+      }
+    }
+  }
+
+  corax_aligned_free(expd);
+  corax_aligned_free(temp);
+
+  for (i = 0; i < rate_cats; ++i)
+    if (tran_evecs[i]) corax_aligned_free(tran_evecs[i]);
+
+  free(tran_evecs);
+  return CORAX_SUCCESS;
+}
+
--- src/corax/kernel/generic/core_pmatrix.c
+++ src/corax/kernel/generic/core_pmatrix.c
@@ -300,7 +300,7 @@ CORAX_EXPORT int corax_core_update_pmatrix(double            **pmatrix,
                                                inv_eigenvecs,
                                                count);
     }
-    if (states == 20)
+    else if (states == 20)
     {
       return corax_core_update_pmatrix_20x20_avx2(pmatrix,
                                                   rate_cats,
@@ -313,6 +313,21 @@ CORAX_EXPORT int corax_core_update_pmatrix(double            **pmatrix,
                                                   eigenvecs,
                                                   inv_eigenvecs,
                                                   count);
+    }
+    else
+    {
+      return corax_core_update_pmatrix_avx2(pmatrix,
+                                            states,
+                                            rate_cats,
+                                            rates,
+                                            branch_lengths,
+                                            matrix_indices,
+                                            params_indices,
+                                            prop_invar,
+                                            eigenvals,
+                                            eigenvecs,
+                                            inv_eigenvecs,
+                                            count);
     }
     /* this line is never called, but should we disable the else case above,
        then states_padded must be set to this value */
