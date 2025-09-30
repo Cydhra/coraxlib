5e39a700500cb02aa12818fee553b9d5fd609cf2
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-09-30 13:46:09 +0200

    implemented AU test

--- src/corax/statistics/au.h
+++ src/corax/statistics/au.h
@@ -7,12 +7,26 @@ extern "C" {
 
 #include "corax/core/common.h"
 
-CORAX_EXPORT void au_p_value(double **replicates,
-                             unsigned int tree,
-                             const double *scales,
-                             const unsigned int *num_replicates,
-                             unsigned int num_scales,
-                             unsigned int num_trees, double initial_threshold);
+/**
+ * Perform the AU test on a single tree, identified by its index in the `replicates` matrix.
+ *
+ * @param replicates normalized and sorted bootstrap replicate statistics
+ * @param tree the index of the test tree in the replicate matrix
+ * @param scales the bootstrap scaling factors
+ * @param num_replicates how many bootstrap replicates where created per scaling factor
+ * @param num_scales how many scaling factors are in the `scales` array
+ * @param initial_threshold the threshold where to begin the convergence test
+ * @param p_value out-parameter for the AU p-value
+ * @return CORAX_SUCCESS if the p-value converged, CORAX_FAILURE otherwise. The p_value parameter will be set in both
+ *         cases.
+ */
+CORAX_EXPORT int au_p_value(double **replicates,
+                            unsigned int tree,
+                            const double *scales,
+                            const unsigned int *num_replicates,
+                            unsigned int num_scales,
+                            double initial_threshold,
+                            double *p_value);
 
 #ifdef __cplusplus
 } /* extern "C" */
