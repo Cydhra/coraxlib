0b78a195d24d170187366880519eff4126dac0ce
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-09-30 11:28:29 +0200

    implemented WLS optimization for d and c

---
+++ src/corax/statistics/au.c
@@ -0,0 +1,140 @@
+#include "au.h"
+
+#include "bootstrap.h"
+#include "stats.h"
+
+/**
+ * The maximum number of iterations for the AU test convergence test until non-convergence is reported. Each iteration
+ * adds one WLS computation and one Newton optimization.
+ */
+const unsigned int CONVERGENCE_ITERATIONS = 100;
+
+/**
+ * Epsilon used for zero-checks in BP and AU tests
+ */
+const double EPS = 1E-16;
+
+/**
+ * Transform an observed Bootstrap Proportion into the linear parameter of the observed distribution (i.e., the
+ * signed distance with curvature correction).
+ */
+double compute_distance(const double bp) {
+    return -normal_quantile(bp);
+}
+
+/**
+ * Solve the Weighted Least Squares system for a set of observed BP values and the model BP distribution analytically.
+ * The function minimizes the loss between observed and the expected distance under normally distributed BP
+ * values and uses those values to obtain estimators for the signed distance and curvature parameters.
+ *
+ * @param bootstrap_counts observed bootstrap counts at each scale
+ * @param scales scaling factors for the multiscale bootstrap
+ * @param scale_roots pre-calculate square roots of the scaling factors
+ * @param num_replicates number of replicates per scaling factor
+ * @param num_scales number of scaling factors in the array
+ * @param d out-parameter for the estimator for d
+ * @param c out-parameter for the estimator for c
+ * @return CORAX_SUCCESS if the WLS system was solved, CORAX_FAILURE if there is no analytical solution. Note that the
+ * parameters d and c are initialized in both cases and can still be used as start parameters to a newton optimization of
+ * c and d.
+ */
+int fit_parameters_wls(const double *const bootstrap_counts, const double *const scales, const double *const scale_roots,
+                       const unsigned int *const num_replicates, const unsigned int num_scales, double* d, double* c) {
+    double* alloc = malloc(sizeof(double) * num_scales * 2);
+    if (!alloc) {
+        // TODO proper handling
+        exit(-1);
+    }
+
+    double* observed_distances = alloc;
+    double* weights = alloc + num_scales;
+
+    unsigned int non_zero_observations = 0;
+    for (unsigned int s = 0; s < num_scales; s++) {
+        const double proportion = bootstrap_counts[s] / (double) num_replicates[s];
+        if (proportion < EPS) {
+            continue;
+        }
+
+        observed_distances[s] = compute_distance(proportion);
+        const double x = normal_pdf(observed_distances[s], 0.0, 1.0);
+        weights[s] = (x * x * (double) num_replicates[s]) / ((1.0 - proportion) * proportion);
+        non_zero_observations++;
+    }
+
+    if (non_zero_observations < 2) {
+        *d = 0;
+        *c = 0;
+        goto clean_fail;
+    }
+
+    // compute model matrix
+    double model_d = 0.0, model_c = 0.0, off_diagonal = 0.0;
+    for (unsigned int s = 0; s < num_scales; s++) {
+        model_d += weights[s] * scales[s];
+        model_c += weights[s] / scales[s];
+        off_diagonal += weights[s];
+    }
+
+    // compute observations
+    double observed_d = 0.0, observed_c = 0.0;
+    for (unsigned int s = 0; s < num_scales; s++) {
+        observed_d += weights[s] * scale_roots[s] * observed_distances[s];
+        observed_c += weights[s] / scale_roots[s] * observed_distances[s];
+    }
+
+    // invert model matrix
+    double determinant = model_d * model_c - off_diagonal * off_diagonal;
+    if (fabs(off_diagonal) == 0.0) {
+        *d = 0.0;
+        *c = 0.0;
+        goto clean_fail;
+    }
+
+    // compute vector product of inverse matrix with observations to solve the system
+    *d = (model_c * observed_d - off_diagonal * observed_c) / determinant;
+    *c = (-off_diagonal * observed_d + model_d * observed_c) / determinant;
+
+    free(alloc);
+    return CORAX_SUCCESS;
+
+clean_fail:
+    free(alloc);
+    return CORAX_FAILURE;
+}
+
+CORAX_EXPORT void au_p_value(double **const replicates,
+                             const unsigned int tree,
+                             double *const scales,
+                             unsigned int *const num_replicates,
+                             const unsigned int num_scales,
+                             const unsigned int num_trees,
+                             const double initial_threshold) {
+    double *counts = malloc(sizeof(double) * num_scales);
+    double *roots = malloc(sizeof(double) * num_scales);
+
+    if (!counts || !roots) {
+        // TODO proper handling
+        exit(-1);
+    }
+
+    // prepare roots of scales so we don't need to compute them as often
+    for (unsigned int s = 0; s < num_scales; s++) {
+        roots[s] = sqrt(scales[s]);
+    }
+
+    double threshold = initial_threshold;
+    for (unsigned int loops = 0; loops < CONVERGENCE_ITERATIONS; loops++) {
+        // obtain smoothed bootstrap counts for current iteration
+        for (unsigned int s = 0; s < num_scales; s++) {
+            counts[s] = corax_empirical_bootstrap_count(replicates[s], num_replicates[s], tree, threshold);
+        }
+
+        double d, c;
+        fit_parameters_wls(counts, scales, roots, num_replicates, num_scales, &d, &c);
+
+    }
+
+
+    free(counts);
+}
