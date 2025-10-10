8c88b101a4932ac76466abfdc21868c1b249fd15
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-10-10 15:56:36 +0200

    make sure to compress resample vectors after resampling to match pattern-compressed site-likelihood vectors

--- src/corax/statistics/bootstrap.h
+++ src/corax/statistics/bootstrap.h
@@ -17,23 +17,29 @@ extern "C" {
  * log-likelihood replicates per tree.
  * The replicates are stored in a matrix, which contains numReplicates replicates per row and one row per tree.
  *
+ * @param rstate the state of a random number generator to generate sample distributions. Note that the state has to be
+ *               equal on all participating threads to generate correct bootstrap replicates.
  * @param replicates the out-parameter for the matrix where to store bootstrap replicates. The matrix needs space for
  *                   num_replicates * num_trees double precision numbers. Alternatively a nullptr can be passed to the
  *                   function, which will allocate the space for the caller. In any case, it is the caller's responsibility
  *                   to free the memory.
  * @param trees_persite_lnl the matrix of per-site log-likelihoods of input trees, one row of likelihoods per tree
- * @param rstate the state of a random number generator to generate sample distributions. Note that the state has to be
- *               equal on all participating threads to generate correct bootstrap replicates.
- * @param num_sites the number of sites of the original alignment (i.e., the length of the per-site lnl vectors)
+ * @param site_weights the per-site weights of pattern compression
+ * @param num_sites_uncompressed the number of sites of the original alignment (i.e., the length of the per-site lnl
+ *                               vectors before pattern compression)
+ * @param num_sites_compressed the number of sites in the alignment after pattern compression (i.e., the length of the
+ *                             site_weights vectors
  * @param num_replicates the number of replicates to generate
  * @param num_trees the number of input trees
  * @param scale the scaling factor of the RELL bootstrap method, 1.0 being canonical RELL bootstrap generating replicates
  *              of the same sequence length.
  */
-CORAX_EXPORT void corax_RELL_bootstrap(double **replicates,
+CORAX_EXPORT void corax_RELL_bootstrap(corax_random_state *rstate,
+                                       double **replicates,
                                        double **trees_persite_lnl,
-                                       corax_random_state *rstate,
-                                       unsigned int num_sites,
+                                       unsigned int *site_weights,
+                                       unsigned int num_sites_uncompressed,
+                                       unsigned int num_sites_compressed,
                                        unsigned int num_replicates,
                                        unsigned int num_trees,
                                        double scale);
@@ -45,26 +51,31 @@ CORAX_EXPORT void corax_RELL_bootstrap(double **replicates,
  * The replicates are stored in matrices, which contain numReplicates replicates per row and one row per tree.
  * There is one matrix per tree.
  *
+ * @param rstate the state of a random number generator to generate sample distributions. Note that the state has to be
+ *               equal on all participating threads to generate correct bootstrap replicates.
  * @param replicate_matrices the out-parameter for an array of matrices of bootstrap replicates. Each matrix needs space
  *                           for num_replicates[i] * num_trees double precision numbers. Alternatively a nullptr can be
  *                           passed to the function, which will allocate the space for the caller. In any case, it is
  *                           the caller's responsibility to free both the matrices, and the pointer array.
  * @param trees_persite_lnl the matrix of per-site log-likelihoods of input trees, one row of likelihoods per tree
- * @param rstate the state of a random number generator to generate sample distributions. Note that the state has to be
- *               equal on all participating threads to generate correct bootstrap replicates.
- * @param num_sites the number of sites of the original alignment (i.e., the length of the per-site lnl vectors)
+ * @param site_weights the per-site weights of pattern compression
+ * @param num_sites_uncompressed the number of sites of the original alignment (i.e., the length of the per-site lnl
+ *                               vectors before pattern compression)
+ * @param num_sites_compressed the number of sites in the alignment after pattern compression (i.e., the length of the
+ *                             site_weights vectors
  * @param num_trees the number of trees in the persite_lnl array
  * @param num_replicates an array defining how many bootstrap replicates to generate per scale
  * @param scales an array of scaling factors for the multiscale bootstrap
  * @param num_scales the number of entries in the scales array.
  */
-CORAX_EXPORT void corax_RELL_multiscale_bootstrap(double ***replicate_matrices,
+CORAX_EXPORT void corax_RELL_multiscale_bootstrap(corax_random_state *rstate,
+                                                  double ***replicate_matrices,
                                                   double **trees_persite_lnl,
-                                                  corax_random_state *rstate,
-                                                  unsigned int num_sites, unsigned int num_trees,
-                                                  const unsigned int *num_replicates,
-                                                  const double *scales,
-                                                  unsigned int num_scales);
+                                                  const unsigned int *site_weights,
+                                                  unsigned int num_sites_uncompressed,
+                                                  unsigned int num_sites_compressed,
+                                                  unsigned int num_trees,
+                                                  const unsigned int *num_replicates, const double *scales, unsigned int num_scales);
 
 /**
  * Normalize a matrix of log-likelihood replicates in such a way that the maximum likelihood replicate of each set of
