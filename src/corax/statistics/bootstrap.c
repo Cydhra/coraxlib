27232ca985544b7e32f11316374b5a82a4270fef
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-10-21 17:47:21 +0200

    make lnl vectors a constant parameter for bootstrapping

--- src/corax/statistics/bootstrap.c
+++ src/corax/statistics/bootstrap.c
@@ -96,7 +96,7 @@ unsigned int bposition(const double *const vector, const double threshold, const
 
 CORAX_EXPORT void corax_RELL_bootstrap(corax_random_state *rstate,
                                        double **replicates,
-                                       double **const trees_persite_lnl,
+                                       const double *const *const trees_persite_lnl,
                                        unsigned int *site_weights,
                                        const unsigned int num_sites_uncompressed,
                                        unsigned int num_sites_compressed,
@@ -142,7 +142,7 @@ CORAX_EXPORT void corax_RELL_bootstrap(corax_random_state *rstate,
 
 CORAX_EXPORT void corax_RELL_multiscale_bootstrap(corax_random_state *rstate,
                                                   double ***replicate_matrices,
-                                                  double **const trees_persite_lnl,
+                                                  const double *const *const trees_persite_lnl,
                                                   const unsigned int *const site_weights,
                                                   const unsigned int num_sites_uncompressed,
                                                   const unsigned int num_sites_compressed,
