2040ae9a38864655bb01cd8db3bad8fcbdfa13cd
Christoph Stelz <christoph.stelz@h-its.org>
2026-01-26 07:31:35 +0100

    Add +I+R test case to freerate unit test

--- test/unit/src/freerate_em.cpp
+++ test/unit/src/freerate_em.cpp
@@ -8,12 +8,13 @@
 #include "corax/tree/utree.h"
 #include "corax/tree/utree_traverse.h"
 #include "environment.hpp"
+#include <cmath>
 #include <gtest/gtest.h>
 #include <numeric>
 
 
 TEST(coraxlib_freerate_em, test1) {
-    const auto sites = 200; // only take a subset of sites
+    const auto sites = 600; // only take a subset of sites
 
     corax_msa_t* msa = corax_phylip_load(env->msa_filename().c_str(), CORAX_TRUE);
     ASSERT_NE(msa, nullptr);
@@ -60,14 +61,17 @@ TEST(coraxlib_freerate_em, test1) {
 
 
     // Restore rates and branch lengths
-    corax_set_category_rates(part, category_rates.data());
-    corax_set_category_weights(part, category_weights.data());
-    for (unsigned int i = 0; i < tree->edge_count; ++i) {
-        corax_treeinfo_set_branch_length(treeinfo, tree->nodes[i], 0.1);
-    }
-    double reset_loglh = corax_treeinfo_compute_loglh(treeinfo, 0);
-    ASSERT_EQ(initial_loglh, reset_loglh);
+    const auto reset_treeinfo = [treeinfo, &category_rates, &category_weights, &part, &tree]() {
+        corax_set_category_rates(part, category_rates.data());
+        part->prop_invar[0] = 0.0;
+        corax_set_category_weights(part, category_weights.data());
+        for (unsigned int i = 0; i < tree->edge_count; ++i) {
+            corax_treeinfo_set_branch_length(treeinfo, tree->nodes[i], 0.1);
+        }
+        return corax_treeinfo_compute_loglh(treeinfo, 0);
+    };
 
+    ASSERT_EQ(initial_loglh, reset_treeinfo());
 
     // Optimize rates with EM algorithm (need multiple rounds of optimization when using Brent)
     double loglh_after_em;
@@ -77,6 +81,39 @@ TEST(coraxlib_freerate_em, test1) {
 
     EXPECT_GT(loglh_after_em, initial_loglh);
     EXPECT_NEAR(loglh_after_em, loglh_after_bfgs, 0.5);
+
+
+    ASSERT_EQ(initial_loglh, reset_treeinfo());
+
+    // Now with invariant
+    double old_loglh;
+    double loglh_after_bfgs_invar = -INFINITY, loglh_after_em_invar = -INFINITY;
+
+    do {
+        old_loglh = loglh_after_bfgs_invar;
+        loglh_after_bfgs_invar = -corax_algo_opt_onedim_treeinfo(treeinfo,
+                                                          CORAX_OPT_PARAM_PINV,
+                                                          CORAX_OPT_MIN_PINV,
+                                                          CORAX_OPT_MAX_PINV,
+                                                          1e-4);
+        loglh_after_bfgs_invar = -corax_algo_opt_rates_weights_treeinfo(treeinfo, CORAX_OPT_MIN_RATE, CORAX_OPT_MAX_RATE, CORAX_OPT_MIN_BRANCH_LEN, CORAX_OPT_MAX_BRANCH_LEN, 0, 1e-4);
+        printf("after bfgs invar: %f\n", loglh_after_bfgs_invar);
+    } while(loglh_after_bfgs_invar - old_loglh > 1e-3);
+    EXPECT_GT(loglh_after_bfgs_invar, initial_loglh);
+
+    do {
+        old_loglh = loglh_after_em_invar;
+        loglh_after_em_invar = -corax_algo_opt_onedim_treeinfo(treeinfo,
+                                                          CORAX_OPT_PARAM_PINV,
+                                                          CORAX_OPT_MIN_PINV,
+                                                          CORAX_OPT_MAX_PINV,
+                                                          1e-4);
+        loglh_after_em_invar = -corax_algo_opt_rates_weights_em_treeinfo(treeinfo, CORAX_OPT_MIN_RATE, CORAX_OPT_MAX_RATE, CORAX_OPT_MIN_BRANCH_LEN, CORAX_OPT_MAX_BRANCH_LEN, 0, 1e-4);
+        printf("after em invar: %f\n", loglh_after_em_invar);
+    } while(loglh_after_em_invar - old_loglh > 1e-3);
+    EXPECT_GT(loglh_after_em_invar, initial_loglh);
+
+    EXPECT_NEAR(loglh_after_em_invar, loglh_after_bfgs_invar, 0.5);
 
 
     corax_partition_destroy(part);
