d0cff8145c81e19271f236fdff660800426ac9ca
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-03-09 11:09:27 +0100

    fix: wrong constant in the normal pdf

--- src/corax/statistics/stats.h
+++ src/corax/statistics/stats.h
@@ -15,7 +15,7 @@ extern "C" {
  * @param stddev standard deviation of the normal distribution
  */
 static inline double normal_pdf(const double x, const double mean, const double stddev) {
-    return 1.0 / (stddev * sqrt(M_2_PI)) * exp(-0.5 * pow((x - mean) / stddev, 2));
+    return 1.0 / (stddev * sqrt(2 * M_PI)) * exp(-0.5 * pow((x - mean) / stddev, 2));
 }
 
 /**
