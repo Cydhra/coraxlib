8af37fae97b49df880793e91cfc60a3e3bf18d16
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-03-09 15:18:30 +0100

    fix: do not subtract two degrees of freedom in newton optimizer since we are checking df < 2 rather than df < 0

--- src/corax/statistics/au.c
+++ src/corax/statistics/au.c
@@ -274,7 +274,6 @@ void fit_parameters_newton(const NewtonOptimizer *const instance,
     const double deriv = normal_pdf(*d - *c, 0.0, 1.0);
     *error = sqrt(deriv * deriv * (-inv_0 - inv_3 + inv_12 + inv_12));
     *p_value = normal_cdf(-(*d - *c), 0.0, 1.0);
-    *df -= 2; // subtract two degrees that we need to estimate c and d.
 }
 
 CORAX_EXPORT int corax_au_p_value(double **const replicates,
